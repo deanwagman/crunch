89f10516235639f6588c95b00ee8dc29223f610d
Dean.Wagman <deanwagman@gmail.com>
2014-06-29 23:43:06 -0400

    Add remove duplicates function

--- scripts/model.js
+++ scripts/model.js
@@ -1,51 +1,88 @@
 /* scripts/model.js */
 
 /* 
-Take in text from user 
+:D Take in text from user 
 
-Sanitize Text
-Remove everything but words
-Put words in array
-remove popular words
-Remove Duplicates
+:D Sanitize Text
+:D Remove everything but words
+:D Put words in array
+:D remove popular words
+:D Remove Duplicates
 Count Mode
-	[word,number of occurrences,word's length]
+    [word,number of occurrences,word's length]
 
 Views
 Use D3.js to Do one of those cool bubble graphs a bubble for every word.
 Depending on the occurrence of the work, the larger it is.
-	Sorting buttons: Occurrenc, length, mood?, 
+    Sorting buttons: Occurrenc, length, mood?, 
 */
 var text, words, COMMONWORDS;
-COMMONWORDS = ["the","be","to","of","and","a","in","that","have","i","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us","is","was","are","were","says","said","has","had"];
+COMMONWORDS = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "i", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us", "is", "was", "are", "were", "says", "said", "has", "had"];
 
-$('#submit').on('click', prepareText);
 
+// Sanitize text from user
+// Remove Everything but words
+// Put Words in Array
 function prepareText() {
-	text = $('#text').val();
-	text = text.toLowerCase().replace(/[^A-z\'-]/g," ");
-	text = text.replace(/\s+/g,",");
-	words = text.split(",");
-	removeCommonWords();
+    text = $('#text').val();
+    text = text.toLowerCase().replace(/[^A-z\'\-]/g, " ");
+    text = text.replace(/\s+/g, ",");
+    words = text.split(",");
+    removeCommonWords();
 }
 
-function removeCommonWords(){
-	var temp, i, j;
-	temp = [];
-	for (i = 0; i < words.length; i++) {
-		for (j = 0; j < COMMONWORDS.length; j++) {
-			if (words[i] == COMMONWORDS[j]) {
-				words[i] = null;
-				break;
-			}
-		}
-		if (words[i]) {
-			temp.push(words[i]);
-		}
-	}
-	words = temp;
-	console.log(words);
+// Remove Popular words
+function removeCommonWords() {
+    var temp, i, j;
+    temp = [];
+    for (i = 0; i < words.length; i++) {
+        for (j = 0; j < COMMONWORDS.length; j++) {
+            if (words[i] == COMMONWORDS[j]) {
+                words[i] = null;
+                break;
+            }
+        }
+        if (words[i]) {
+            temp.push(words[i]);
+        }
+    }
+    words = temp;
+    removeDuplicates();
+}
+
+// Remove and Count Duplicates
+// Organized in an Array of [word, number of times]
+function removeDuplicates() {
+    var i, temp;
+    temp = [];
+    // Go through each word
+    for (i = 0; i < words.length; i++) {
+        // If the word is a key in the temp array then increase the value
+        // Else add it to the temp array as a key then assign the value to 1
+        if (temp[words[i]]) {
+            temp[words[i]]++;
+        } else {
+            temp[words[i]] = 1;
+        }
+    }
+    // Clear out the word array to be reorganized
+    words = [];
+
+    // Push every item in temp array to the word array as [word, count]
+    var key;
+    for (key in temp) {
+        if (temp.hasOwnProperty(key)) {
+            words.push([ key, temp[key]]);
+        }
+    }
+    console.log(words);
 }
 
 
+
+
+
+
+// Take in Text from user
+$('#submit').on('click', prepareText);
 
